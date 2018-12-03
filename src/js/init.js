0ac83251abd31f1f0951f294af404da38c329449
maksymenko-o <maksymenko-o@live.com>
2018-12-03 21:46:50 +0200

    menu changes

--- src/js/init.js
+++ src/js/init.js
@@ -3,7 +3,7 @@ $(function() {
     (function () {
         let nav = $('.main-menu__nav');
 
-        $('.header__menu-btn').click(function() {
+        $('.menu-btn').click(function() {
             if(nav.hasClass('active')) {
                 nav.removeClass('active');
             }
